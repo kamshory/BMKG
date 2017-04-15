e8280bdbb1fad491d5cb54dce3e02c6627eb0da8
Kamshory, MT <kamshory@yahoo.com>
2017-04-15 14:35:42 +0700

    Update weather.php

--- weather.php
+++ weather.php
@@ -1,9 +1,10 @@
 <?php
+$province_id = substr(trim(strip_tags(@$_GET['province_id'])), 0, 2);
 require('lib/bmkg.php');
 
 $bmkg = new BMKG();
 
-$data = $bmkg->weather();
+$data = $bmkg->weather($province_id);
 
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
